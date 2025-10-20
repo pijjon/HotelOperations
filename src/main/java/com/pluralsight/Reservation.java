d09257ab8b328b91a9f384310d8085c87c2d88c9
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-20 15:14:41 -0400

    define Reservation.java properties

---
+++ src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private double basePrice;
+    private boolean isWeekend;
+
+}
