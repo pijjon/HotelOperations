093d4528d328562ea51c9f307058d5ed3770cf4b
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-21 09:31:33 -0400

    define Reservation.java methods

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,9 +1,53 @@
 package com.pluralsight;
 
+import java.util.Objects;
+
 public class Reservation {
     private String roomType;
     private int numberOfNights;
-    private double basePrice;
+    private final double basePrice;
     private boolean isWeekend;
 
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.basePrice = (roomType.equals("king")) ? 139.00 : 124.00;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        double output = this.basePrice * this.numberOfNights;
+        if (this.isWeekend) {
+            return output * 1.1;
+        }
+        return output;
+    }
 }
